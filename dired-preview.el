753d88fa52d4ac593a0951929a3b178883a4fcc6
Protesilaos <info@protesilaos.com>
2026-04-24 13:26:48 +0300

    Use "Protesilaos" instead of "Protesilaos Stavrou"
    As I announced here: <https://protesilaos.com/news/2026-04-21-omitting-the-surname/>.

--- dired-preview.el
+++ dired-preview.el
@@ -2,8 +2,8 @@
 
 ;; Copyright (C) 2023-2026  Free Software Foundation, Inc.
 
-;; Author: Protesilaos Stavrou <info@protesilaos.com>
-;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
+;; Author: Protesilaos <info@protesilaos.com>
+;; Maintainer: Protesilaos <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/dired-preview
 ;; Version: 0.6.0
 ;; Package-Requires: ((emacs "28.1"))
