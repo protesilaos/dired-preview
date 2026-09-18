a015765e537622a662df9da45898c5d989b117dc
Protesilaos <info@protesilaos.com>
2026-09-18 20:43:57 +0300

    Upgrade to dired-preview version 0.7.0

--- dired-preview.el
+++ dired-preview.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos <info@protesilaos.com>
 ;; Maintainer: Protesilaos <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/dired-preview
-;; Version: 0.6.2
+;; Version: 0.7.0
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: files, convenience
 
